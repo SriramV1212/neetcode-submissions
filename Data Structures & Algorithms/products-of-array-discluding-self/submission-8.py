36f87a0d64eb9757a7296436f12e5f03889be8b9
Sriram V <152197781+SriramV1212@users.noreply.github.com>
2026-08-06 18:45:52 -0400

    Add: products-of-array-discluding-self - submission-8

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-8.py
@@ -0,0 +1,35 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+
+        product , zer_count = 1,0
+
+        res=[]
+
+        for n in nums:
+            if n==0:
+                zer_count+=1
+                continue
+            product*=n
+
+        if zer_count > 1:
+            return([0] * len(nums))
+
+        if zer_count == 1:
+            for n in nums:
+                if n==0:
+                    res.append(product)
+                res.append(0)
+            
+            return(res)
+        
+        for n in nums:
+            res.append(product//n)
+        
+        return(res)
+
+        
+
+
+
+        
+
