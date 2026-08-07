4317d8dd6c2c43293bdf95f2cfc0d375ebdb3bc8
Sriram V <152197781+SriramV1212@users.noreply.github.com>
2026-08-06 22:20:05 -0400

    Add: is-palindrome - submission-13

---
+++ Data Structures & Algorithms/is-palindrome/submission-13.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        forward = []
+
+        for char in s:
+            if char.isalnum():
+                forward.append(char.lower())
+        
+        backward = []
+
+        for i in range(len(s)-1,-1,-1):
+            if s[i].isalnum():
+                backward.append(s[i].lower())
+
+        print("".join(forward), "".join(backward) )
+
+
+        return ("".join(forward) == "".join(backward))
+        
