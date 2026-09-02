4d0b9e0a81aea8833af9d749c89f0e35390a72b0
Sriram V <152197781+SriramV1212@users.noreply.github.com>
2026-09-02 16:29:52 -0400

    Add: foreign-dictionary - submission-1

---
+++ Data Structures & Algorithms/foreign-dictionary/submission-1.py
@@ -0,0 +1,58 @@
+class Solution:
+    def foreignDictionary(self, words: List[str]) -> str:
+
+        graph = defaultdict(list)
+
+        for i in range(len(words)-1):
+            word = words[i]
+            next_word = words[i+1]
+
+            left = 0
+            right = 0
+
+            while left < len(word) and right < len(next_word):
+                if word[left] !=  next_word[right]:
+                    graph[word[left]].append(next_word[right])
+                    break
+
+                left+=1
+                right+=1
+
+            if right == len(next_word) and len(next_word) < len(word):
+                return ""
+
+        res = []
+
+        visited = set()
+
+        def dfs(s):
+            if s in visited:
+                return
+
+            visited.add(s)
+
+            res.append(s)
+
+            if graph[s] == []:
+                return
+
+            for next_char in graph[s]:
+                dfs(next_char)
+
+        dfs(words[0][0])
+
+        return "".join(res)
+
+            
+
+
+
+            
+
+            
+
+
+
+
+
+        
