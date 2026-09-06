dee2a60a8c75f2ffeec4c963e4487ee942e3ddc3
Sriram V <152197781+SriramV1212@users.noreply.github.com>
2026-09-05 21:11:14 -0400

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.py
@@ -0,0 +1,24 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        dp = [-1] * len(nums)
+
+        dp[0] = nums[0]
+        dp[1] = nums[1]
+
+        def recurse(house):
+            if dp[house] != -1:
+                return dp[house]
+
+            if house <= 1:
+                return nums[house]
+
+            dp[house] = max(recurse(house-1), nums[house] + recurse(house-2))
+            return dp[house]
+
+        recurse(len(nums)-1)
+        recurse(len(nums)-2)
+
+
+        amt = max(dp)
+
+        return amt
